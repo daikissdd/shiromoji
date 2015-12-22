ff967fb7fd121e2aff6411ceaaefbd9a79d1aaf4
daikissdd <daikissdd@gmail.com>
2015-12-22 17:38:05 +0900

    update engin

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-var kuromoji = require('kuromoji');
-var _ = require('lodash');
-var S = require('string');
-var clearText = require('./lib/clean-ja-text.js');
-var splitText = require('./lib/split-ja-text.js');
-var pickKeyword = require('./lib/pick-keyword.js');
-var isTrash = require('./lib/is-trash.js');
-
-var shiromoji = function(text, callback) {
-	var dic = {dicPath: './node_modules/kuromoji/dist/dict/'};
-	kuromoji.builder(dic).build(function(err, tokenizer) {
-		if (err) return callback(err, null);
-		var _keywords = [];
-		_.each(splitText(clearText(text)), function(t) {
-			var kuromojiWords = tokenizer.tokenize(t);
-			_keywords.push(pickKeyword(kuromojiWords));
-		});
-		
-		var keywords = [];
-		_.each(_.flatten(_keywords, true), function(keyword) {
-			if (!isTrash(keyword)) keywords.push(keyword);
-		});
-		callback(null, keywords);
-	});
-};
-
-shiromoji.count = require('./lib/keyword-count');
-shiromoji.countAll = require('./lib/keyword-count-all');
+var shiromoji = require('./lib/shiromoji/');
+shiromoji.count = require('./lib/count/');
+shiromoji.countAll = require('./lib/count-all/');
 
 module.exports = shiromoji;
